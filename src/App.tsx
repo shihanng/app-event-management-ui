a3b3e38360eac39685b4ea67ed7cc1443dd157ad
Shi Han NG <shihanng@gmail.com>
2021-01-09 10:24:19 +0900

    Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import tw from "twin.macro";
 import Event from "./components/Event";
+import Header from "./components/Header";
 import Login from "./components/Login";
-import Logout from "./components/Logout";
 import useToken from "./hooks/useToken";
 
 const queryClient = new QueryClient();
 
-const Div = tw.div`mt-2 mb-2`;
+const Div = tw.div`mt-2 mb-2 w-full mx-auto max-w-screen-md`;
 
 function App() {
   const { token, setToken } = useToken();
@@ -20,7 +20,7 @@ function App() {
     <Div>
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
-          <Logout token={token} setToken={setToken} />
+          <Header token={token} setToken={setToken}></Header>
           <Switch>
             {token ? (
               <Route path="/" render={() => <Event token={token} />} />
---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import tw from "twin.macro";
+import Logout, { LogoutProps } from "./Logout";
+
+const TwHeader = tw.header`flex justify-between`;
+const Title = tw.h1`text-2xl`;
+
+interface Props extends LogoutProps {}
+
+const Header: React.FC<Props> = ({ token, setToken }) => {
+  return (
+    <TwHeader>
+      <Title>Event Management</Title>
+      <Logout token={token} setToken={setToken} />
+    </TwHeader>
+  );
+};
+
+export default Header;
